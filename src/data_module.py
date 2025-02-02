7d253030e5fdf807bdf632dd3a00db8a21934a37
Parth Sinha <sinhaparth555@gmail.com>
2025-02-02 18:20:40 +0000

    updated data module and fix the errro

--- src/data_module.py
+++ src/data_module.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 from transformers import AutoTokenizer
 import json
+import os
 
 class QADataset(Dataset):
     def __init__(self, data, tokenizer, max_length):
@@ -25,9 +26,9 @@ class QADataset(Dataset):
             return_tensors='pt'
         )
         return {
-            'input_ids': encoding['input_ids'].squeeze().T,
+            'input_ids': encoding['input_ids'].squeeze(),
             'attention_mask': encoding['attention_mask'].squeeze(),
-            'labels': encoding['labels'].squeeze().T
+            'labels': encoding['labels'].squeeze()
         }
         
 class QADataModule(pl.LightningDataModule):
@@ -37,16 +38,32 @@ class QADataModule(pl.LightningDataModule):
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
         self.batch_size = batch_size
         self.max_length = max_length
+        self.num_workers = min(4, os.cpu_count() or 1) if torch.cuda.is_available() else 0
+        self.pin_memory = torch.cuda.is_available()
         
     def setup(self, stage=None):
         with open(self.data_path) as f:
             data = json.load(f)
             dataset = QADataset(data, self.tokenizer, self.max_length)
-            self.train_data, self.val_data = torch.utils.data.random_split(dataset, [0.9, 0.1])
+            train_size = int(0.9 * len(dataset))
+            val_size = len(dataset) - train_size
+            self.train_data, self.val_data = torch.utils.data.random_split(
+                dataset, [train_size, val_size]
+            )
     
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.batch_size, shuffle=True, num_workers=15)
+        return DataLoader(
+            self.train_data, 
+            batch_size=self.batch_size, 
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory
+        )
     
     def val_dataloader(self):
-        return DataLoader(self.val_data, batch_size=self.batch_size, num_workers=15)
-    
+        return DataLoader(
+            self.val_data, 
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory
+        )
